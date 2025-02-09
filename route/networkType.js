569abec50248b44a96615735d395c98064dabd74
joshwebpoll <joshharyomide@gmail.com>
2025-02-09 12:39:29 +0100

    add middleware for  network route

--- route/networkType.js
+++ route/networkType.js
@@ -3,10 +3,11 @@ const {
   createNetwork,
   getNetwork,
 } = require("../controllers/networkController");
+const { protectedRoute } = require("../controllers/userAuthController");
 
 const router = express.Router();
 
-router.post("/network", createNetwork);
-router.get("/network", getNetwork);
+router.post("/network", protectedRoute, createNetwork);
+router.get("/network", protectedRoute, getNetwork);
 
 module.exports = router;
