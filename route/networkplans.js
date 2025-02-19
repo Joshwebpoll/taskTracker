5404d3ddb2879e165e908fa81878ad5e15a726bb
joshwebpoll <joshharyomide@gmail.com>
2025-02-19 12:44:46 +0100

    update route with middleware

--- route/networkplans.js
+++ route/networkplans.js
@@ -4,11 +4,12 @@ const {
   getPlans,
   getPlansByNetwork,
 } = require("../controllers/networkPlansController");
+const protectRoute = require("../middlewares/protectRoute");
 
 const router = express.Router();
 
-router.post("/plans", createPlans);
-router.get("/plans", getPlans);
-router.get("/plansnet/", getPlansByNetwork);
+router.post("/plans", protectRoute, createPlans);
+router.get("/plans", protectRoute, getPlans);
+router.get("/plansnet/", protectRoute, getPlansByNetwork);
 
 module.exports = router;
